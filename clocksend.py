b523081c0095ff50bd293681fda40aa488eda4ad
itconor <conor.ewings@gmail.com>
2018-10-15 16:12:26 +0100

    Updated to Fix Spamming UDP

--- clocksend.py
+++ clocksend.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 import socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -9,15 +9,13 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 data = ("AIR1:ON")
 data2 = ("AIR1:OFF")
+datas = [data2, data]
+lastst = None
 
-try:
-    while True:
-        if GPIO.input(17) == True:
-            client_socket.sendto(data, ("127.0.0.1",3310))
-            time.sleep(.4)
-        else:
-            client_socket.sendto(data2, ("127.0.0.1",3310))
-        time.sleep(.4)
-
-except KeyboardInterrupt:  
-    GPIO.cleanup()         # clean up after yourself  
+while True:
+    sleep(.4)
+    newst = GPIO.input(17)
+    if newst == lastst:
+        continue
+    lastst = newst
+    client_socket.sendto(datas[newst], ("127.0.0.1", 3310))
